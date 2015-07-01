bcf83fd8ad01dea0eda310199ecc54243c8ea2ab
Steven Osburn <osburn6469@gmail.com>
2015-06-30 21:43:21 -0600

    Day 1 Updates

---
+++ C# Fundamentals/Day1_Homework Assignmnet/Day1_Homework Assignmnet/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1_Homework_Assignmnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String fused = "12 23 34 45";
+            Console.WriteLine("Please enter the sequence to stop the timer: ");
+            string userinput = Console.ReadLine();
+
+            if (userinput==fused)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("You saved the world!!!");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.Beep(5000, 1000);
+                Console.WriteLine("FAILURE!!!");
+            }
+        }
+    }
+}
