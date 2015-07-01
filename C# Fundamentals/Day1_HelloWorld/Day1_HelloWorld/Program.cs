cb54d3a5aa78a14f7a49c3201865d2b1eab623fb
Steven Osburn <osburn6469@gmail.com>
2015-06-30 21:35:20 -0600

    Day 1 Updates

---
+++ C# Fundamentals/Day1_HelloWorld/Day1_HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");    //System.Cosonle.Writeline yould be required if we didn't include System at the top.
+            Console.ReadLine();
+        }
+    }
+}
