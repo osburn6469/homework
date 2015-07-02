c14e3157e49499c59832f98afd2068a1157e2f29
Steven Osburn <osburn6469@gmail.com>
2015-07-02 07:59:56 -0600

    Day 2 Updates

---
+++ UnderstandingOOP/UnderstandingOOP/Auto.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandingOOP
+{
+    class Auto
+    {
+        public string Make;
+        public string Model;
+        public int Year;
+        public string Color;
+        public int Miles;
+        public int Speed;
+
+        public void Accelerate()
+        {
+            Speed++;
+            Console.WriteLine("Current speed: " + Speed.ToString());
+        }
+
+        public void Deccelerate()
+        {
+            Speed--;
+            Console.WriteLine("Current speed: " + Speed.ToString());
+        }
+
+
+        public int AccelerateInt(int increasedSpeed)
+        {
+            Speed+= increasedSpeed;
+            Console.WriteLine("Current speed: " + Speed.ToString());
+            return Speed;
+        }
+
+        public int DeccelerateInt(int decreasedSpeed)
+        {
+            Speed-= decreasedSpeed;
+            Console.WriteLine("Current speed: " + Speed.ToString());
+            return Speed;
+        }
+        public string SpeedLimitViolation(string initialMessage, int speedLimit)
+        {
+            string message = null;
+            if (Speed < speedLimit)
+            {
+                message = "";
+            }
+            else
+            {
+                message = "You are going to FAST!!!!";
+            }
+            return initialMessage + " " + message;
+        }
+
+    }
+}
---
+++ UnderstandingOOP/UnderstandingOOP/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandingOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Auto myCar = new Auto();
+
+            myCar.Speed = 0;
+            myCar.Make = "Dodge";
+            myCar.Model = "Journey";
+            myCar.Year = 2010;
+            myCar.Color = "Silver";
+            myCar.Miles = 50000;
+
+            Console.WriteLine(myCar.Make + " " + myCar.Model);
+            myCar.Accelerate();
+            myCar.Accelerate();
+            myCar.Accelerate();
+            myCar.Accelerate();
+            myCar.Deccelerate();
+
+            myCar.AccelerateInt(5);
+            myCar.AccelerateInt(2);
+            myCar.AccelerateInt(1);
+            myCar.AccelerateInt(3);
+            myCar.DeccelerateInt(6);
+
+            string message = myCar.SpeedLimitViolation("Warning: ", 20);
+            Console.WriteLine("My current speed: " + myCar.Speed);
+            Console.WriteLine(message);
+            Console.ReadLine();
+        }
+    }
+}
